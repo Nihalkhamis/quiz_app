2302b199900ab8d17129d2a826d5cc63ed935c30
Inova <inova@Ewailys-Mac-mini.local>
2022-08-14 11:23:32 +0200

    Initial commit

---
+++ lib/answer.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+       // style: ButtonStyle(
+       //   backgroundColor: MaterialStateProperty.all(Colors.blue),
+       //   foregroundColor: MaterialStateProperty.all(Colors.white),
+        //),
+        style: ElevatedButton.styleFrom(primary: Colors.blue, onPrimary: Colors.white),
+
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
